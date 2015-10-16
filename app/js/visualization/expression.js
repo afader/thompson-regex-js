f5dbdc23e3506ce52356992ce1055007acad8fd5
Anthony Fader <afader@gmail.com>
2015-10-16 11:22:46 -0700

    cleaning up

--- app/js/visualization/expression.js
+++ app/js/visualization/expression.js
@@ -1,19 +1,16 @@
 var exprs = require('../../../lib/exprs.js');
 
 var visGraph = function(rootExpr) {
-  // This function converts an expression tree to a vis.js graph. The strategy used is to 
-  // evaluate the expression tree. The evaluation of each node in the tree mutates a vis.js 
-  // graph by adding nodes and edges.
   var nodes = [];
   var edges = [];
   var graph = {nodes: nodes, edges: edges};
-
-  // Next define some helper functions to generate unique node ids and mutate the graph.
   var id = 0;
-  var nextId = function() { return id++ };
-  var newNode = function(label, level) {
-    level = level == null ? 0 : level;
-    var node = { id: nextId(), label: label, level: level };
+  var nextId = () => id++;
+  var newNode = function(label, expression) {
+    var node = { id: nextId(), label: label, level: 0 };
+    if (expression && 'parse' in expression) {
+      node.parse = { start: expression.parse.start, end: expression.parse.end };
+    }
     nodes.push(node);
     return node;
   };
@@ -25,41 +22,25 @@ var visGraph = function(rootExpr) {
     });
     return newEdges;
   };
-
-  // The first case is when encountering a leaf node like whitespace or wildcard. In this case,
-  // we want to create a new node in the graph that has the name of the leaf node. It is returned
-  // so the parent expression has access to it. 
-  var leafNode = function(name) {
-    return function() {
-      return newNode(name)
+  var leafNode = name => () => newNode(name, this.expression);
+  var innerNode = name => function(...toNodes) {
+    var fromNode = newNode(name, this.expression);
+    addEdges(fromNode, toNodes);
+    var childLevels = toNodes.map(n => n.level);
+    fromNode.level = Math.max.apply(this, childLevels) + 1;
+    return fromNode;
+  };
+  var quoteNode = function(expr) {
+    if (exprs.isAtom(expr)) {
+      return newNode(expr.value, this.expression);
+    } else {
+      var childNode = exprs.evaluate(expr, env);
+      var createNode = innerNode('quote').bind(this);
+      return createNode(childNode);
     }
   };
-  // The second case is when encountering an inner node like a concatenation. In this case, we want
-  // to create a new node and edges going from the new node to the nodes corresponding to the 
-  // child expressions. Because inner nodes may have one or more argument expressions, we need to
-  // access them via the javascript arguments object. 
-  var innerNode = function(name) {
-    return function() {
-      var fromNode = newNode(name);
-      var toNodes = exprs.getArgs(arguments);
-      addEdges(fromNode, toNodes);
-      fromNode.level = Math.min.apply(this, toNodes.map(function(x) { return x.level })) - 1;
-      return fromNode;
-    }
-  };
-  // Now we define the environment for evaluation. The environment is a table mapping symbols that
-  // appear in expressions to values. In this case, all of the values in the symbol table are
-  // functions that produce the side-effect of modifying the vis.js graph.
   var env = {
-    quote: function(x) {
-      var arg = x[0];
-      if (exprs.isAtom(arg)) {
-	return newNode(arg);
-      } else {
-	var child = exprs.evaluate(arg, env);
-	return innerNode('quote')(child);
-      }
-    },
+    quote: quoteNode,
     charEquals: innerNode('charEquals'),
     inRange: innerNode('inRange'),
     predicate: innerNode('predicate'),
@@ -77,17 +58,14 @@ var visGraph = function(rootExpr) {
     digit: leafNode('digit'),
     wildcard: leafNode('wildcard')
   };
-  // Calls the eval/apply loop on the root expression. Since we're only interested in the
-  // side-effect of building the expression graph
   exprs.evaluate(rootExpr, env);
   return graph;
 };
 
-// Object to configure the vis.js display of the graph.
 var config = {
   layout: {
     hierarchical: {
-      direction: 'UD',
+      direction: 'DU',
       levelSeparation: 100
     }
   },
